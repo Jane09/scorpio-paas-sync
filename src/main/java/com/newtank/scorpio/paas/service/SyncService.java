8eea1497cfab7d6c0916b1710d69004e534088af
zhoujian <zhoujian@newtank.cn>
2018-10-29 14:17:45 +0800

    初始化

--- src/main/java/com/newtank/scorpio/paas/service/SyncService.java
+++ src/main/java/com/newtank/scorpio/paas/service/SyncService.java
@@ -82,6 +82,7 @@ public class SyncService {
                                 //校验白羊座的数据是否存在
                                 AriesCustomer ariesCustomer = ariesDao.findByTenantIdAndMobile(tenantId,mobile);
                                 if(ariesCustomer != null){
+                                    //TODO only sync data which does not exists.
                                     log.info("电话： {} 对应的客户已经存在，更新数据",mobile);
                                     AriesCustomerUpdate update = new AriesCustomerUpdate();
                                     update.setId(ariesCustomer.getId());
@@ -104,7 +105,7 @@ public class SyncService {
                                         checkBlocked(custLead,tenantId,mobile);
                                         custLead.setCustomer_id(ariesCustomer.getId());
                                         custLead.setData_source(customer.getSource());
-                                        custLead.setProduct_name(customer.getMarket_project());
+//                                        custLead.setProduct_name(customer.getMarket_project());
                                         custLead.setRes_id(customer.getRes_id());
                                         ariesDao.addCustomerLead(custLead);
                                     }
@@ -140,7 +141,7 @@ public class SyncService {
                                     checkBlocked(custLead,tenantId,mobile);
                                     custLead.setCustomer_id(ariesCustomer.getId());
                                     custLead.setData_source(customer.getSource());
-                                    custLead.setProduct_name(customer.getMarket_project());
+//                                    custLead.setProduct_name(customer.getMarket_project());
                                     custLead.setRes_id(customer.getRes_id());
 
                                     ariesDao.addCustomerLead(custLead);
